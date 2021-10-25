9a5be90eadcb731a40f259e62c9603dd38a59fc8
sung_jee <85841791+kim-sung-jee@users.noreply.github.com>
2021-10-25 12:47:33 +0900

    Update 1764.cpp

--- baekjoon/silver/5/1764.cpp
+++ baekjoon/silver/5/1764.cpp
@@ -1,110 +1,52 @@
 #include<iostream>
 #include<list>
+#include<algorithm>
+#include<vector>
 using namespace std;
-
-void move(list<string>&s){
-    string temp;
-    temp=s.front();
-    s.push_back(temp);
-    s.pop_front();
-}
 
 
 int main(){
     
     int N,M;
     cin>>N>>M;
-    int max;
-    if(N>=M){
-        max=N;
-    }else{
-        max=M;
-    }
-    string *array=new string[max];
-    list<string> list1;
-    list<string> list2;
+    
+
+    vector<string> v1;
+    vector<string> v2;
     string a;
     for(int i=0;i<N;i++){
         cin>>a;
-        list1.push_back(a);
+        v1.push_back(a);
     }
-    for(int i=0;i<M;i++){
-        cin>>a;
-        list2.push_back(a);
-    }
-    // list1.sort();
-    // list2.sort();
-    int z=0;
-    for(int i=0;i<max;i++){
-
-        if(list1.front()==list2.front()){
-            array[z]=list1.front();
-            z++;
-            list1.pop_front();list2.pop_front();
-        }else if((list1.front()!=list2.front())){
-
-        if(list1.size()>=list2.size()){
-            for(int t=0;t<list1.size();t++){
-                move(list1);
-                if(list1.front()==list2.front()){
-                    array[z]=list1.front();
-                    z++;
-                    list2.pop_front();
-                    break;
-                }
-            }
-            list1.pop_front();
-        }else{
-            for(int t=0;t<list2.size();t++){
-                move(list2);
-                if(list1.front()==list2.front()){
-                    array[z]=list1.front();
-                    z++;
-                    list1.pop_front();
-                    
-                    break;
-                }
-            }
-            list2.pop_front();
-            //cout<<list2.front();
-        }
-
+    sort(v1.begin(),v1.end());    
+    
+    string b;
+    vector<string>::iterator iter,iter2;
+    iter=v1.begin();
+    iter2=v1.end();
+    bool torf;
+     for(int i=0;i<M;i++){
+        cin>>b;
+        if(binary_search(iter,iter2,b)){
+            v2.push_back(b);
         }
     }
-    if(z==0){
+    int t=v2.size();
+    
+    sort(v2.begin(),v2.end());
+    if(t==0){
         cout<<"0";
     }else{
-        cout<<z<<endl;
+        cout<<t<<endl;
     }
-    for(int i=0;i<z-1;i++){
-        cout<<array[i]<<endl;
+
+    string c[t];
+    for(int i=0;i<t;i++){
+        c[t-i-1]=v2.back();
+        v2.pop_back();
     }
-    cout<<array[z-1];
-
-
-
-    // int z=0;
-    // int size=list1.size();
-    // list<string>::iterator iter=list2.begin();
-    // for(int i=0;i<size;i++){
-    //     for(int t=0;t<list2.size();t++){
-    //         if(list1.front()==*iter  ){
-    //             array[z]=list1.front();
-    //             z++;
-    //             break;
-    //         }
-    //         iter++;
-    //     }
-    //     list1.pop_front();
-    //     iter=list2.begin();
-    // }
-    // if(z==0){
-    //     cout<<"0";
-    // }else{
-    //     cout<<z<<endl;
-    // }
-    // for(int i=0;i<z-1;i++){
-    //     cout<<array[i]<<endl;
-    // }
-    // cout<<array[z-1];
+    for(int i=0;i<t-1;i++){
+        cout<<c[i]<<endl;
+    }
+    cout<<c[t-1];
 }
