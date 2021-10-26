55f0502c1d4bc5b99937956d1c61bac353c86850
sung_jee <85841791+kim-sung-jee@users.noreply.github.com>
2021-10-26 17:47:58 +0900

    연속부분최대곱(다이나믹프로그래밍)

--- baekjoon/silver/4/2670.cpp
+++ baekjoon/silver/4/2670.cpp
@@ -1,19 +1,8 @@
 #include<iostream>
 #include<algorithm>
 using namespace std;
-double aaa(double *array,int n,int t){
-    double a=1;
-    double a2[t];
-    for(int i=0;i<t;i++){
-        for(int z=0;z<n;z++){
-            a*=*(array+z+i);
-        }
-        a2[i]=a;
-        a=1;
-    }
-    sort(a2,a2+t);
-    return a2[t-1];
-}
+
+
 int main(){
     int N;
     cin>>N;
@@ -23,11 +12,22 @@ int main(){
         cin>>a;
         array[i]=a;
     }
-    double *array2=new double[N-1];
-    for(int i=2;i<N+1;i++){
-        array2[i-2]=aaa(array,i,N+1-i);
+   
+    double maxi=-1;
+    double *array2=new double[N];
+    for(int i=0;i<N;i++){
+        if(i==0){
+            array2[i]=array[i];
+        }else{
+         
+            array2[i]=max(array[i],array2[i-1]*array[i]);
+        }
+        maxi=max(maxi,array2[i]);
     }
-    sort(array2,array2+N-1);
-    cout<<array2[N-2];
 
-}   
+    cout<<fixed;
+    cout.precision(3);
+    cout<<maxi;
+    
+
+}
