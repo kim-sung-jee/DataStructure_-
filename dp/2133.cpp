f8f085d94be7d9ff581075cd83d37984690bc80c
sung_jee <85841791+kim-sung-jee@users.noreply.github.com>
2022-01-25 04:43:16 +0900

    타일채우기
    예외가 발생할 때 생각해보기

---
+++ dp/2133.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+int N;
+
+int dp[31];
+
+
+int main(){
+    cin>>N;
+    
+    dp[0]=1;
+    dp[2]=3;
+    for(int i=4;i<=N;i+=2){
+
+        dp[i]=dp[i-2]*3;
+        for(int j=4;j<=i;j+=2){
+            dp[i]+=dp[i-j]*2;
+        }
+    }
+    cout<<dp[N];
+}
