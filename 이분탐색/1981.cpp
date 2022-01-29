2229947f532ff1da158c04133d71bd92079f9ff4
sung_jee <85841791+kim-sung-jee@users.noreply.github.com>
2022-01-29 10:14:02 +0900

    배열에서 이동
    처음보는 이분탐색 문제였따.
    (0,0) 에서 (n-1.n-1)까지 이동하는데 만난 최대값과 최소값의 차이가 가장 작아지는 경우를 구하기
    초기 start 와 end 를 start=0(아직 모르니깐),end를=maxi-mini로 설정해주고 이분탐색을 시작한다.
    이때 25번째 줄의 if문을 통해 각 노드들이 mid값을 유지한 채로 이동할 수 있는지 설정해준다.
    18번째 줄의 fort문은 mini 부터 maxi 까지 S를 유지한채 그래프의 노드들을 검사한다.
    ((0,5)와 (1,6))
    한가지 궁금했던 점은 중복방문할 가능성 이였는데 
    새로 방문하는 지점이 생기면 maxi가 커지거나 mini가 줄어들 가능성만 생기므로 
    안된다..

---
+++ 이분탐색/1981.cpp
@@ -0,0 +1,85 @@
+#include<iostream>
+#include<queue>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int dx[4]={1,-1,0,0};
+int dy[4]={0,0,1,-1};
+int N;
+int mini=200;
+int maxi=0;
+vector<int> g[100];
+bool visited[100][100];
+bool bfs(int S){
+    queue<pair<int,int>> q;
+    // mini ~ maxi 까지 다해본다.
+    // 다해봤는데 없으면 false 반환.
+    for(int i=mini;i<=maxi;i++){
+        for(int j=0;j<100;j++){
+            fill_n(visited[j],100,true);
+        }
+        for(int j=0;j<N;j++){
+            for(int t=0;t<N;t++){
+                //mid 값(S) 즉 차이를 유지한 채로 이동할 수 있는지
+                if(i<=g[j][t]&&g[j][t]<=i+S){
+                    visited[j][t]=false;
+                }
+            }
+        }
+
+        q.push({0,0});
+
+        while(!q.empty()){
+            int x=q.front().first;
+            int y=q.front().second;
+            q.pop();
+            if(visited[y][x]==true){
+                continue;
+            }
+            visited[y][x]=true;
+            if(x==N-1&&y==N-1){
+                return true;
+            }
+            for(int i=0;i<4;i++){
+                int nx=x+dx[i];
+                int ny=y+dy[i];
+                if(0<=nx&&nx<N&&0<=ny&&ny<N){
+                    q.push({nx,ny});
+                }
+            }
+
+
+        }
+
+    }
+    return false;
+}
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin>>N;
+    int a;
+    for(int i=0;i<N;i++){
+        for(int t=0;t<N;t++){
+            cin>>a;
+            g[i].push_back(a);
+            maxi=max(maxi,a);
+            mini=min(mini,a);
+        }
+    }
+    int Start=0;
+    int End=maxi-mini;
+    int Mid;
+    while(Start<=End){
+        Mid=(Start+End)/2;
+        if(bfs(Mid)==true){
+            End=Mid-1;
+        }else{
+            Start=Mid+1;
+        }
+    }
+    cout<<Start;
+    
+    
+}
